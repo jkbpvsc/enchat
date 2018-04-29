fc22b456d98e2556896a5f3f1ccffa30ca56916e
Jakob Povšič <jakob.povsic@gmail.com>
2018-04-29 10:53:43 +0200

    const port

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import { createServer } from 'http'
 import indexRouter from './router/index'
 import * as SocketIOServer from './socket/SocketIOServer';
 
+const PORT = process.env.PORT || 80;
+
 const app = express();
 const server = createServer(app);
 SocketIOServer.init(server);
@@ -14,9 +16,7 @@ app.use(express.static('public'));
 app.use('/', indexRouter);
 app.set('view engine', 'pug');
 app.use(morgan('dev'));
-app.set('port', 80);
 
+app.set('port', PORT);
 
-server.listen(app.get('port'));
-//app.listen(3000, () => console.log('App running'));
-
+server.listen(app.get('port'), () => console.log('App running on port: ' + PORT));
